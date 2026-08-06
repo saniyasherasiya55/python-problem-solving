6eda7d1b896a8b5310187353c805c3b6f0e80c62
saniyasherasiya55 <sherasiyasaniya00@gmail.com>
2026-08-06 12:57:27 +0530

    Create p1.py

---
+++ unit-2/p1.py
@@ -0,0 +1,22 @@
+# 1. Write a program to demonstrate conditional statements using if if-else and if-elif-else. 
+
+
+num=int(input("enter the number:"))
+
+if num >0:
+     print(" number is positive")
+
+if num % 2 == 0:
+     print("number is even ")
+
+else:
+    print("number is odd ")
+
+if num>0:
+     print("number is positive")
+
+elif num<0:
+     print("number is negative")
+
+else:
+    print("zero")
