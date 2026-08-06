9056e09f9d98b2ebf9dbfb7c154060e5ba4080db
saniyasherasiya55 <sherasiyasaniya00@gmail.com>
2026-08-06 13:06:18 +0530

    Create 8.py

---
+++ unit-2/8.py
@@ -0,0 +1,11 @@
+# Write a program to illustrate variable scope using local global and nonlocal variables. 
+
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+even = {x for x in numbers if x % 2 == 0}
+print("Set :", even)
+
+
+
+
+
