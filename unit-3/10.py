e2ad11b705315f7a7db7f5dec59b95cc00c0fc26
saniyasherasiya55 <sherasiyasaniya00@gmail.com>
2026-08-20 12:52:09 +0530

    Initial commit

--- unit-3/10.py
+++ unit-3/10.py
@@ -7,9 +7,9 @@ text ="""
             age:20 email:
             abc@gmail.com
                                         """
-name=re.search(r"name:\s*(.*)",text ).group(1)
-age=re.search(r"age:\s*(\d+*)",text ).group(1)
-email=re.search(r"email:\s*(\s+*)",text ).group(1)
+name=re.search(r"name:\s*(.*)", text ).group(1)
+age=re.search(r"age:\s*(\d+)", text ).group(1)
+email=re.search(r"email:\s*(\S+)", text ).group(1)
 
 print("name:" ,name)
 print("age:" ,age)
---
+++ unit-3/ex_1.py
@@ -0,0 +1,5 @@
+Prog = re.compile()
+str='cat mat rat'
+result=prog.search(str)
+
+print(result.group())
